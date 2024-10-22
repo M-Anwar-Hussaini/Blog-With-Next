a8304a35e193f5e2b175ce373bb43bcabb776fda
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-22 12:26:48 +0430

    Update the logo name

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="flex items-center justify-between py-5 max-w-2xl mx-auto">
       <Link href="/" className="font-bold text-3xl">
-        Marshal<span className="text-primary">Blog</span>
+        Scientific<span className="text-primary">Blog</span>
       </Link>
       <ModeToggle />
     </nav>
