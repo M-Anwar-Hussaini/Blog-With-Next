3cd58cdedb7192a7c820f0ad35e9a561d7ab7644
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-22 10:42:30 +0430

    Create a server action to fetch all the blogs

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <div>Home</div>;
+import { SimpleBlogCard } from '@/lib/interface';
+import { client } from '@/lib/sanity';
+import { defineQuery } from 'next-sanity';
+
+async function fetchData() {
+  const query = defineQuery(`
+      *[_type == 'blog'] | order(createaAt desc) {
+      title,
+      smallDescription,
+      'currentSlug': slug.current,
+      titleImage
+      }
+    `);
+  const data: SimpleBlogCard[] = await client.fetch(query);
+  return data;
+}
+
+export default async function Home() {
+  const data = await fetchData();
+  console.log(data);
+  return <>here some another text</>;
 }
