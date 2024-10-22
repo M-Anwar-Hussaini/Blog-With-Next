43e72551c1b1e891249eb7c0bb25dfc48f513205
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-22 20:08:18 +0430

    Add some padding to the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from './ModeToggle';
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between py-5 max-w-2xl mx-auto">
+    <nav className="flex items-center justify-between py-5 max-w-2xl mx-auto px-2">
       <Link href="/" className="font-bold text-3xl">
         Scientific<span className="text-primary">Blog</span>
       </Link>
